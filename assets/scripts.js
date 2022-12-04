a28e1be62fe97ab95de70c5405f1194f7ef2e490
unknown <stevendepauw@rocketmail.com>
2022-12-03 22:36:27 -0600

    main page html and css mostly formatted

---
+++ assets/scripts.js
@@ -0,0 +1,44 @@
+let questions = [
+    "Commonly used data types DO NOT include:", 
+    "The conditions in an if/else statement is enclosed within ______.",
+    "Arrays in JavaScript can be used to store  ______.",
+    "String values must be enclosed within _____ when being assigned to variables.",
+    "A very useful tool used during development and debugging for printing to the debugger is:",
+]
+
+let options = [
+    [
+        "1. strings", 
+        "2. booleans",
+        "3. alerts", 
+        "4. numbers",
+    ],
+
+    [
+        "1. quotes", 
+        "2. curly brackets",
+        "3. parenthesis",
+        "4. square brackets",
+    ],
+
+    [
+        "1. numbers and strings", 
+        "2. other arrays",
+        "3. booleans",
+        "4. all of the above",
+    ],
+
+    [
+        "1. commas", 
+        "2. curly brackets",
+        "3. quotes",
+        "4. parenthesis",
+    ],
+
+    [
+        "1. JavaScript", 
+        "2. terminal/bash",
+        "3. for loops",
+        "4. console.log",
+    ],
+]
